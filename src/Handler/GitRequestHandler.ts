8a4ff4dbdbdfce5f83ac7672d1ded08da2aaf635
timoCasti <timo.castelberg@stud.unibas.ch>
2022-05-19 11:56:26 +0200

    created GitRequestHandler.ts which catches all http requests including regex .git/ does not handle it yet, need to be forwarded to git-backend implementation.

---
+++ src/Handler/GitRequestHandler.ts
@@ -0,0 +1,69 @@
+import escapeStringRegexp from 'escape-string-regexp';
+import type { HttpHandlerInput, HttpRequest } from '@solid/community-server';
+import { HttpHandler, getLoggerFor, NotImplementedHttpError } from '@solid/community-server';
+
+/**
+ * Handler that serves static resources on specific paths.
+ * Relative file paths are assumed to be relative to cwd.
+ * Relative file paths can be preceded by `@css:`, e.g. `@css:foo/bar`,
+ * in case they need to be relative to the module root.
+ */
+export class GitRequestHandler extends HttpHandler {
+  private readonly logger = getLoggerFor(this);
+
+  /**
+     * Creates a handler for the provided static resources.
+     * @param assets - A mapping from URL paths to paths,
+     *  where URL paths ending in a slash are interpreted as entire folders.
+     * @param options - Cache expiration time in seconds.
+     */
+  public constructor() {
+    super();
+  }
+
+  /**
+     * Creates a regular expression that matches the URL paths.
+     */
+  private createPathMatcher(assets: Record<string, string>): RegExp {
+    // Sort longest paths first to ensure the longest match has priority
+    const paths = Object.keys(assets).sort((pathA, pathB): number => pathB.length - pathA.length);
+
+    // Collect regular expressions for files and folders separately
+    const files = ['.^'];
+    const folders = ['.^'];
+    for (const path of paths) {
+      (path.endsWith('/') ? folders : files).push(escapeStringRegexp(path));
+    }
+
+    // Either match an exact document or a file within a folder (stripping the query string)
+    return new RegExp(`^(?:(${files.join('|')})|(${folders.join('|')})([^?]+))(?:\\?.*)?$`, 'u');
+  }
+
+  public async gitRegexFinder({ url }: HttpRequest): Promise<string> {
+    const exp = /\/*.git\/$/u;
+    const match = exp.exec(url ?? '');
+    if (!match) {
+      throw new NotImplementedHttpError(`No Git resource found at ${url}`);
+    }
+
+    return 'ok';
+  }
+
+  public async canHandle({ request }: HttpHandlerInput): Promise<void> {
+    if (request.method !== 'GET' && request.method !== 'POST') {
+      throw new NotImplementedHttpError('Only GET and HEAD requests are supported');
+    }
+    const isGit = await this.gitRegexFinder(request);
+    if (!isGit) {
+      throw new NotImplementedHttpError('Not Git');
+    }
+  }
+
+  public async handle({ request, response }: HttpHandlerInput): Promise<void> {
+    // Determine the asset to serve
+    const rex = this.gitRegexFinder(request);
+    // eslint-disable-next-line @typescript-eslint/no-base-to-string, no-console
+    console.log(`handle GitHandler${rex}`);
+    response.end();
+  }
+}
